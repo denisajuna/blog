63f7e8005b262fa9777258fd5264693c0835bf24
Petr Juna <petr.juna@gmail.com>
2024-05-09 12:17:59 +0200

    chore: url

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,7 @@ const whenExternalScripts = (items = []) =>
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://denisajuna.github.io/blog/',
   output: 'static',
 
   integrations: [
